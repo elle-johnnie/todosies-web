d9161666968b6b3a967f7203848250438a818cf0
LJ <liz.johnson.email@gmail.com>
2020-05-07 22:54:39 -0700

    Add signed out links

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import AuthLinks from './AuthLinks'
+import NoAuthLinks from './NoAuthLinks'
+
 
 const NavBar = () => {
     return(
         <nav className="nav wrapper grey darken-2">
             <div className="container">
-            <Link to='/' classname='brand-logo'>Todosies</Link>
-            <AuthLinks />
+                <Link to='/' classname='brand-logo'>Todosies</Link>
+                <AuthLinks />
+                <NoAuthLinks />
             </div>
         </nav>
     )
