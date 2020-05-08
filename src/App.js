530f30319cd78eb8c27249faf0851671747bca0d
LJ <liz.johnson.email@gmail.com>
2020-05-08 13:16:59 -0700

    create sign in and sign up components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './Components/Layout/NavBar'
 import Dashboard from './Components/Dashboard/Dashboard'
 import ProjectDetail from './Components/Project/ProjectDetail'
+import SignIn from './Components/Auth/SignIn';
+import SignUp from './Components/Auth/SignUp';
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
           <Switch> {/* switch allows only one route at a time on the page, otherwise loads them all if called */}
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetail} />
+            <Route path='/signin' component={SignIn} />
+            <Route past='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
