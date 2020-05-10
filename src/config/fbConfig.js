ba8f0823770fa24c7fdc4d71eef078bb1066b9f9
LJ <liz.johnson.email@gmail.com>
2020-05-09 18:25:19 -0700

    add firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgrhJCaqxjf9JqtY4sXaI9m0GjbzcVBUo",
+    authDomain: "todosies-web.firebaseapp.com",
+    databaseURL: "https://todosies-web.firebaseio.com",
+    projectId: "todosies-web",
+    storageBucket: "todosies-web.appspot.com",
+    messagingSenderId: "257773271607",
+    appId: "1:257773271607:web:6fe83ae00d0f1a59b49db2",
+    measurementId: "G-80JJ3YGN8V"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true })
+
+
+  export default firebase;
