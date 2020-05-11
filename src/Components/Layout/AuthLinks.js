bad5245664d56208c82caaa0fd7292ac64edef3b
LJ <liz.johnson.email@gmail.com>
2020-05-11 10:29:50 -0700

    enable signout action

--- src/Components/Layout/AuthLinks.js
+++ src/Components/Layout/AuthLinks.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signOut } from '../../Store/Actions/authActions'
 
-const AuthLinks = () => {
+const AuthLinks = (props) => {
     return(
        <ul className="right">
-           <li><NavLink to='/create'>New Project</NavLink></li>
-           <li><NavLink to='/'>Log Out</NavLink></li>
+           <li><NavLink className="btn green" to='/create'>New Project</NavLink></li>
+           <li><button className="btn grey" onClick={props.signOut}>Log Out</button></li>
            <li><NavLink to='/' className="btn btn-floating pink">LJ</NavLink></li>
        </ul>
     )
 }
 
-export default AuthLinks
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(mapDispatchToProps)(AuthLinks)
