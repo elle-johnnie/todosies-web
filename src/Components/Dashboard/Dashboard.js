88069b125ec50ebabf0a12f200dc408af635e923
LJ <liz.johnson.email@gmail.com>
2020-05-10 17:02:05 -0700

    connect to firestore via firestoreConnect

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../Project/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+
 
 class Dashboard extends Component {
     render () {
@@ -25,9 +28,13 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+    console.log(state);
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+    firestoreConnect(['projects']),
+    connect(mapStateToProps))
+(Dashboard)
