6024f76cd05844ce6205235976e476083745d214
LJ <liz.johnson.email@gmail.com>
2020-05-11 21:39:40 -0700

    connect notifications list

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-
+const admin =require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,4 +8,36 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello LJ from Firebase!");
 });
 
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('notification added', doc));
+})
 
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate(doc => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName}  ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+    return createNotification(notification);
+    })
+
+
+exports.userJoins = functions.auth.user()
+    .onCreate(user => admin.firestore().collection('users')
+        .doc(user.uid).get()
+        .then(doc => {
+            const newUser = doc.data();
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.firstName}  ${newUser.lastName}`,
+                time: admin.firestore.FieldValue.serverTimestamp()
+            };
+
+        return createNotification(notification);
+    }));
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
     render () {
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
         if (!auth.uid) { return<Redirect to="/signin/" />; }
         return (
             <div className="dashboard container">
@@ -20,7 +20,7 @@ class Dashboard extends Component {
                     </div>
                     {/*  right side col notifications offset by 1 col */}
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -32,11 +32,15 @@ const mapStateToProps = (state) => {
     console.log(state);
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth, 
+        notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
-    firestoreConnect(['projects']),
+    firestoreConnect([
+        { collection: 'projects' },
+        { collection: 'notifications', limit: 3} 
+    ]),
     connect(mapStateToProps))
 (Dashboard)
